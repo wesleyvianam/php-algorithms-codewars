b81b4ad4413fa12f782db2c3fe772b618370a3e7
wesley viana martins <wesley.vmartins.js@gmail.com>
2023-01-06 20:49:50 -0300

    longest

---
+++ 02-longest.php
@@ -0,0 +1,22 @@
+<?php
+/*
+
+Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible,
+containing distinct letters - each taken only once - coming from s1 or s2.
+
+functions:
+    array_unique(): ensures that there is only one specific item in the array
+    str_split(): convert a string to an array
+    asort(): Sort an array in ascending order and maintain index association
+    implode(): transform array in string
+*/
+
+function longest($a, $b) 
+{
+    $letters = array_unique(str_split($a .= $b));
+    asort($letters);
+    return implode('', $letters);
+}
+
+echo longest("aretheyhere", "yestheyarehere");
+
