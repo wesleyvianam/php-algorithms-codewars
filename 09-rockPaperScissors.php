e6251775383201d4e190677d68c86a5df56451ab
wesley viana martins <wesley.vmartins.js@gmail.com>
2023-01-17 11:22:37 -0300

    rock paper scissors

---
+++ 09-rockPaperScissors.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+
+Let's play! You have to return which player won! In case of a draw return Draw!.
+
+Kata training: https://www.codewars.com/kata/5672a98bdbdd995fad00000f/train/php
+
+*/
+
+function rpc($p1, $p2) {
+    if ($p1 === $p2) return "Draw!";
+
+    if ($p1 == "scissors" && $p2 == "paper" ||
+        $p1 == "rock" && $p2 == "scissors" ||
+        $p1 == "paper" && $p2 == "rock"
+    ) { return "Player 1 won!"; }
+
+    return "Player 2 won!";
+}
+
+echo rpc("scissors", "paper");
