c09cd778a147185e2419350d4e145aa2c036bf48
Wesley Viana Martins <wesley.vmartins.js@gmail.com>
2023-01-27 08:06:48 -0800

    refactor

--- Fundamentals/PHP/17-removeArchorFromUrl.php
+++ Fundamentals/PHP/17-removeArchorFromUrl.php
@@ -10,6 +10,7 @@ Complete the function/method so that it returns the url with anything after the 
 "www.codewars.com?page=1" -->"www.codewars.com?page=1"
 
 Kata training: https://www.codewars.com/kata/51f2b4448cadf20ed0000386/train/php
+
 */
 
 function replaceAll(string $string): string
