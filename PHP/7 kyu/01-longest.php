037a9046dcf23b81fd01a1cb57d5469bf5a5384d
Wesley Viana Martins <wesley.vmartins.js@gmail.com>
2023-01-31 22:41:06 -0800

    refactor

--- PHP/7 kyu/01-longest.php
+++ PHP/7 kyu/01-longest.php
@@ -9,6 +9,8 @@ functions:
     str_split(): convert a string to an array
     asort(): Sort an array in ascending order and maintain index association
     implode(): transform array in string
+
+    https://www.codewars.com/kata/5656b6906de340bd1b0000ac/train/php
 */
 
 function longest($a, $b) 
