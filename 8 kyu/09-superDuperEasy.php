e40fb47491e87d4c92a836eb78b43ea918834abf
Wesley Viana <wesley.vmartins.js@gmail.com>
2023-02-01 07:25:57 -0300

    super duper easy

---
+++ 8 kyu/09-superDuperEasy.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+
+Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+
+Kata Training: https://www.codewars.com/kata/55a5bfaa756cfede78000026/train/php
+
+*/
+
+function problem($x)
+{
+    //your code here
+}
+
+ECHO problem(1);
