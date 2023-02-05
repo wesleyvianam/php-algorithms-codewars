08e0ad8147fd000fc929226ce42f321eceb1d562
Wesley Viana <wesley.vmartins.js@gmail.com>
2023-02-05 15:47:11 -0300

    string repeat

--- 8 kyu/15-stringRepeat.php
+++ 8 kyu/15-stringRepeat.php
@@ -12,7 +12,7 @@ Kata Training: https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/train/php
 
 */
 
-function repeatStr($n, $str)
+function repeatStr(int $n, string $str): string
 {
     return str_repeat($str, $n);
 }
