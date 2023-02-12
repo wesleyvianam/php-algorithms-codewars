25ea5cb0f0be64facba8f927be827618ee656777
Wesley Viana <wesley.vmartins.js@gmail.com>
2023-02-12 15:53:36 -0300

    type

--- 7 kyu/03-numberOfPeopleInTheBus.php
+++ 7 kyu/03-numberOfPeopleInTheBus.php
@@ -20,7 +20,8 @@ Kata training https://www.codewars.com/kata/5648b12ce68d9daa6b000099/train/php
 
 */
 
-function number($bus_stops) {
+function number(array $bus_stops): int
+{
     $in_bus = 0;
     
     foreach ($bus_stops as $bus) {
