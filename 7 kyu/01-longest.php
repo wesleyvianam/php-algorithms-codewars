aef54182c875543de35d673923794b21a80dd4c2
Wesley Viana <wesley.vmartins.js@gmail.com>
2023-02-14 15:54:43 -0300

    type

--- 7 kyu/01-longest.php
+++ 7 kyu/01-longest.php
@@ -13,7 +13,7 @@ functions:
     https://www.codewars.com/kata/5656b6906de340bd1b0000ac/train/php
 */
 
-function longest($a, $b) 
+function longest(string $a, string $b): string
 {
     $letters = array_unique(str_split($a .= $b));
     asort($letters);
