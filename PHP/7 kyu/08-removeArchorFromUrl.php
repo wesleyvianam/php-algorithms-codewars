21e9b036882c41c046b3e4d381480191e5ff56d7
Wesley Viana Martins <wesley.vmartins.js@gmail.com>
2023-02-16 21:14:52 -0800

    .

--- PHP/7 kyu/08-removeArchorFromUrl.php
+++ PHP/7 kyu/08-removeArchorFromUrl.php
@@ -15,7 +15,7 @@ Kata training: https://www.codewars.com/kata/51f2b4448cadf20ed0000386/train/php
 
 function replaceAll(string $string): string
 {
-    return explode("#", $string)[0];
+    return explode("#", $string)[0]; 
 }
 
 echo replaceAll("www.codewars.com#about");
