497acfa41f450635362b0ceead00f208da6e0b95
Wesley Viana Martins <wesley.vmartins.js@gmail.com>
2023-03-15 05:33:46 -0300

    array diff

---
+++ 6 kyu/04-arrayDiff.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+
+Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+It should remove all values from list a, which are present in list b keeping their order.
+
+arrayDiff([1,2],[1]) == [2]
+If a value is present in b, all of its occurrences must be removed from the other:
+
+arrayDiff([1,2,2,2,3],[2]) == [1,3]
+
+*/
+
+function arrayDiff(array $a, array $b): array
+{
+    return array_values(array_diff($a, $b));
+}
+
+echo arrayDiff([1, 2, 3], [1,2]);
