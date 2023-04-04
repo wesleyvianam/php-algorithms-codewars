9ee65a19c809ceeca1769e9e65ce16afca49e128
wesley viana martins <wesley.vmartins.js@gmail.com>
2023-04-04 15:24:12 -0300

    inicio do da resolução desse algoritimo para finalizar mais tarde

---
+++ 8 kyu/19-ANeedleInTheHaystack.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+
+Can you find the needle in the haystack?
+
+Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+After your function finds the needle it should return a message (as a string) that says:
+
+"found the needle at position " plus the index it found the needle, so:
+
+Example(Input --> Output)
+
+Kata Training: https://www.codewars.com/kata/56676e8fabd2d1ff3000000c/train/php
+
+*/
+
+function findNeedle(array $haystack)
+{
+
+
+    return strpos($haystack, 'needle');
+}
+
+echo findNeedle(['3', '123124234', null, 'needle', 'world', 'hay', 2, '3', true, false]);
