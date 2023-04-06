607bbe21199fc8cb4335aebff5c434671ad6967a
wesley viana martins <wesley.vmartins.js@gmail.com>
2023-04-06 10:27:33 -0300

    A needle In The Haystack

--- 8 kyu/19-ANeedleInTheHaystack.php
+++ 8 kyu/19-ANeedleInTheHaystack.php
@@ -16,11 +16,9 @@ Kata Training: https://www.codewars.com/kata/56676e8fabd2d1ff3000000c/train/php
 
 */
 
-function findNeedle(array $haystack)
+function findNeedle(array $haystack): string
 {
-
-
-    return strpos($haystack, 'needle');
+    return "found the needle at position " . array_search("needle", $haystack);
 }
 
 echo findNeedle(['3', '123124234', null, 'needle', 'world', 'hay', 2, '3', true, false]);
