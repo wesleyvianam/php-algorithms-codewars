2218a2c6198867e6104fa465787fdea8e2965a38
Wesley Viana Martins <wesley.vmartins.js@gmail.com>
2023-05-02 04:45:09 -0300

    drink about

---
+++ 8 kyu/21-drinkAbout.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+
+Children under 14 old.
+Teens under 18 old.
+Young under 21 old.
+Adults have 21 or more.
+
+Kata Training: https://www.codewars.com/kata/56170e844da7c6f647000063/train/php
+
+*/
+
+function people_with_age_drink(int $old): string {
+    switch (true) {
+        case ($old < 14): return "drink toddy";
+        case ($old < 18): return "drink coke";
+        case ($old < 21): return "drink beer";
+        case ($old >= 21): return "drink whisky";
+    }
+    return "";
+}
+
+echo people_with_age_drink(19);
