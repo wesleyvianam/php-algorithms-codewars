dc16fb2b0a1bf8a1744bfde6a2e10b3ed02329f2
wesley <wesley.vmartins.js@gmail.com>
2023-09-22 15:47:13 -0300

    Save skeleton algorithm

---
+++ 4 Kyu/04 Vigenère Cipher Helper.php
@@ -0,0 +1,17 @@
+<?php
+
+// Kata Training: https://www.codewars.com/kata/52d1bd3694d26f8d6e0000d3/train/php
+
+class VigenereCipher
+{
+    public function encode($s)
+    {
+    }
+
+    public function decode($s)
+    {
+    }
+}
+
+
+
